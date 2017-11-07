1aaf7cd3549d6080d78f92850b91fb3296834f3f
Riley Dillon <riley@14four.com>
2017-11-07 10:17:32 -0800

    updated the resizing

--- src/Carousel3d.vue
+++ src/Carousel3d.vue
@@ -413,8 +413,9 @@
                 this.viewport = this.$el.clientWidth
             },
             setSize () {
-                this.$el.style.cssText += 'height:' + this.slideHeight + 'px;'
-                this.$el.childNodes[0].style.cssText += 'width:' + this.slideWidth + 'px;' + ' height:' + this.slideHeight + 'px;'
+                this.$el.style.cssText += 'height:' + this.containerHeight + 'px;'
+                this.$el.childNodes[0].style.cssText += 'height:' + this.slideHeight + 'px;'
+                this.$el.childNodes[0].childNodes[0].style.cssText += 'width:' + this.slideWidth + 'px;' + ' height:' + this.slideHeight + 'px;'
             }
         },
 
